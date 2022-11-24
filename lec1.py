fe10eaa0e68275fbf1ca451d17848f1b6c6c9b14
Kate <vvellspring@gmail.com>
2022-11-24 15:47:19 +0300

    Lection 1 - initial commit

---
+++ lec1.py
@@ -0,0 +1,15 @@
+# print('Hello world')
+# value = None
+a = 123
+b = 1.23
+# print(a)
+# print(b)
+# value = 12333
+# print(type(value))
+s = 'qwerty'
+# print(s) # вывод строки
+print(a, b, s)
+print(a, '-', b, '-', s)
+print('{} - {} - {}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
